3b298817d7932ab511eeac7f0af3d7b764efeea9
themousepotato <navaneeths1998@gmail.com>
2018-11-10 01:38:09 +0530

    Add function to get attributes of user for #5

--- kwoc/leaderboard.py
+++ kwoc/leaderboard.py
@@ -5,39 +5,57 @@ import os
 
 import utils
 
-def get_commits(username, repo_name):
+def get_attributes(username, repo_name):
     '''
-    username : 'kshitij10496'
-    repo_name : 'kshitij10496/IIKH'
+    ------- INPUT ----------------------
+    username: 'kshitij10496'
+    repo_name: 'kshitij10496/IIKH'
                 'sympy/sympy'
-    '''
-    # total_commits = 0
-    # total_additions, total_deletions, total_changes = 0, 0, 0
 
-    # user = g.get_user(username)
-    # repo = g.get_repo(repo_name)
-    # starting_date = datetime.datetime(2016, 5, 1)
-    # ending_date = datetime.datetime(2016, 12, 31)
-    # all_commits = repo.get_commits(author=username)#, since=starting_date, until=ending_date)
-    # for commit in all_commits:
-    #     total_additions += int(commit.stats.additions)
-    #     total_deletions += int(commit.stats.deletions)
-    #     total_changes += int(commit.stats.total)
-    #     total_commits += 1
-    # # return total_commits
-    # print("Total commits: " + str(total_commits))
-    # print("Total additions: " + str(total_additions))
-    # print("Total deletions: " + str(total_deletions))
-    # print("Total changes: " + str(total_changes))
-    query = "https://api.github.com/repos/{}/stats/contributors?access_token={}".format(repo_name,os.environ["DEFCON_GITHUB_AUTH_TOKEN"])
+    ------- OUTPUT ---------------------
+    attr = {'commits':60,
+            'additions':10000,
+            'deletions':100,
+            'prs_open':10,
+            'prs_closed':5,
+            'prs_merged':3}
+    '''
+
+    attr = {'commits':0,
+            'additions':0,
+            'deletions':0,
+            'prs_open':0,
+            'prs_closed':0,
+            'prs_merged':0}
+
+    query = 'https://api.github.com/repos/{}/stats/contributors?access_token={}'.format(repo_name,os.getenv('GITHUB_TOKEN'))
     response = requests.get(query).json()
-    commits = 0
-    try :
-        for data in response :
-            if data["author"]["login"].lower() == username.lower() :
-                commits+=int(data["total"])
-        return commits
-    except TypeError :
-        msg = "Unable to get commits in {} repo.\nFollowing error occured : {}".format(repo_name,traceback.format_exc())
+
+    try:
+        for data in response:
+            if data['author']['login'].lower() == username.lower():
+                attr['commits'] += int(data['total'])
+                attr['additions'] += sum(int(week['a']) for week in data['weeks'])
+                attr['deletions'] += sum(int(week['d']) for week in data['weeks'])
+
+    except TypeError:
+        msg = 'Unable to get attributes in {} repo.\nFollowing error occured : {}'.format(repo_name,traceback.format_exc())
         utils.slack_notification(msg)
         return 0
+
+    query = 'https://api.github.com/repos/{}/pulls?state=all&access_token={}'.format(repo_name,os.getenv('GITHUB_TOKEN'))
+    response = requests.get(query).json()
+
+    try:
+        for data in response:
+            if data['user']['login'].lower() == username.lower():
+                attr['prs_open'] += 1
+                attr['prs_closed'] += 1 if data['closed_at'] else 0
+                attr['prs_merged'] += 1 if data['merged_at'] else 0
+
+    except TypeError:
+        msg = 'Unable to get attributes in {} repo.\nFollowing error occured : {}'.format(repo_name,traceback.format_exc())
+        utils.slack_notification(msg)
+        return 0
+
+    return attr
