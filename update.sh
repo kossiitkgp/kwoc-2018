650d28e732dbe631da4772a299c91d9b355cc0fb
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-12-11 15:42:05 +0530

    update auto-update script

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-set git = "/usr/bin/git"
+alias git = "/usr/bin/git"
 cd /home/kwoc/kwoc
-$git add .
-$git stash
-$git fetch origin
-$git rebase origin/master
-$git stash apply
+git add .
+git stash
+git fetch origin
+git rebase origin/master
+git stash apply
 /usr/bin/sudo /bin/systemctl restart kwoc.service
 echo "KWoC Service Restarted!"
