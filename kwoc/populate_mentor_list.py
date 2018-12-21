3cdabb30bb91e909ec0bdf8026b6968f373aa77d
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-12-21 14:25:02 +0530

    fix wrong encoding for generating mentor script

--- kwoc/populate_mentor_list.py
+++ kwoc/populate_mentor_list.py
@@ -14,12 +14,12 @@ MENTORS_JSON = os.path.join(root_dir,'gh_scraper/list_of_mentors.json')
 def main():
     mentors = []
 
-    with open(PROJECT_CSV, 'r') as f:
+    with open(PROJECT_CSV, 'r', encoding='utf-8') as f:
         table = csv.reader(f)
         for row in table:
             mentors.append('{mentor} | {project}'.format(mentor=row[0], project=row[2]))
 
-    with open(MENTORS_JSON, 'w') as f:
+    with open(MENTORS_JSON, 'w', encoding='utf-8') as f:
         mentors = mentors[1:]
         mentors = list(set(mentors))
         mentors.sort()
