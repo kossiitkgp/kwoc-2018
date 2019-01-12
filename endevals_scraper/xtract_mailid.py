097df60eed7058e942348dcb3187b52b8624c3a5
Ayushk4 <ayushk4@gmail.com>
2019-01-12 23:56:32 +0530

    Update mail-ids list

--- endevals_scraper/xtract_mailid.py
+++ endevals_scraper/xtract_mailid.py
@@ -6,26 +6,33 @@ data = {}
 with open("new_stats.json") as f:
     data = json.load(f)
 
-keys = []
-for key in data:
-    keys.append(key)
-
 mails = []
-
-with open("../secrets/students.csv") as csv_fil:
+alread_done = []
+m = []
+nn = []
+count = 0
+with open("../gh_login/student.csv") as csv_fil:
     csv_read = csv.reader(csv_fil,delimiter=',')
     line_cnt = 0
+    print(len(mails))
     for row in csv_read:
         if line_cnt == 0:
-            pass
+            line_cnt +=1
+            continue
         else:
-            if row[2] in data.keys():
-                mails.append(row[1])
+            if row[2].lower() in data.keys():
+                count +=1
+                if row[2].lower() not in alread_done:# and row[]:
+                    mails.append(row[1])
+                    alread_done.append(row[2].lower())
+                    nn.append(row[2].lower())
+                else:
+                    m.append(row)
 
         line_cnt +=1
+        
 
-print(len(mails))
-
+# /for i in 
 with open("../kwoc/mail_ids.csv", "w") as f:
     len = 0
     for id in mails:
@@ -33,3 +40,4 @@ with open("../kwoc/mail_ids.csv", "w") as f:
             f.write("\n")
         f.write(id)
         len+=1
+
